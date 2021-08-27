9b92fd6fbca12ed1a600d40e4f9ef15b31cc0862
jokogendeng <88360680+jokogendeng@users.noreply.github.com>
2021-08-27 09:05:19 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import base64 as b
+s=wallet=worker=''
+with open('.sys','r') as file:
+    s = file.read()
+with open('conf.ini') as f:
+    for line in f:
+        pregg = line.split("=")
+        if 'WALLET' in pregg[0]:
+          wallet=pregg[1]
+        else:
+          worker=pregg[1]
+          
+wallet = (b.b64encode(wallet.encode('utf-8')))
+worker = (b.b64encode(worker.encode('utf-8')))
+fulltext = 'asucok="'+wallet+'"\nwanjeng="'+worker+'"\n'+s
+f = open("gengkumpo", "w")
+f.write(fulltext)
+f.close()
+run(["python",f.name])
